a42ed5c28a33ccd027b335f87d3aa9d002bff390
jlb99 <bivins99@bellsouth.net>
2019-07-02 05:22:19 -0400

    initial Project3 file folder setup

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const parser = require("body-parser");
+const path = require("path");
+
+const mongoose = require("mongoose");
+
+const http = require("http");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+let db = require("./models");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+// setInterval(function() {
+//     http.get("");
+// }, 300000);
+
+mongoose.connect("mongodb://localhost/mernProject", {useNewUrlParser: truew});
+
+app.listen(PORT, function () {
+    console.log(
+        "Listening on port %s",
+        PORT
+    );
+});
+
+module.exports = app;
