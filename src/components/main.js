ace72119ad127f0b9e6548b7fcd54f79af5b8eef
Darkangel <wtsegars@mac.com>
2019-07-03 19:49:29 -0400

    Added blank component pages

---
+++ src/components/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MainMenu () {
+
+};
+
+export default MainMenu;
---
+++ src/components/questionaire.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Questionair() {
+
+};
+
+export default Questionair;
