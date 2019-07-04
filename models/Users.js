ea5ca501573c30ba851ca7eb984737371cc2ab5a
Darkangel <wtsegars@mac.com>
2019-07-03 20:47:09 -0400

    Updated the Users Model

--- models/Users.js
+++ models/Users.js
@@ -11,10 +11,6 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
-    email: {
-        type: String,
-        required: true
-    },
     questionairs: {
         type: Schema.Types.ObjectId,
         ref: "Questions"
