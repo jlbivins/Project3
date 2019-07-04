eb4eb08607bfd6ca02cb77ef80436ba7540aae07
Darkangel <wtsegars@mac.com>
2019-07-03 21:48:22 -0400

    Started to work on the sign in component

---
+++ src/components/signinform.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class SignInForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        
+    }
+
+    handleSubmit(event) {
+
+    }
+}
+
+export default SignInForm;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import SignInForm from '../components/signinform';
 
 function Login() {
-
+  return (
+      <SignInForm />
+  );
 };
 
 export default Login;
