a0b7e9caf4af30d8e30c2f4bfb2efaa16276e099
Darkangel <wtsegars@mac.com>
2019-07-05 23:11:13 -0400

    Updated login component

--- src/components/signinform.js
+++ src/components/signinform.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class SignInForm extends React.Component {
+class SignInForm extends Component {
     constructor(props) {
         super(props);
         this.state ={
-
+            username: "",
+            password: ""
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
@@ -12,11 +13,49 @@ class SignInForm extends React.Component {
     }
 
     handleInputChange(event) {
-        
+        if (event.target.name==="username"){
+            this.setState({
+                username: event.target.username
+            });
+        } else if (event.target.name==="password"){
+            this.setState({
+                password: event.target.password
+            });
+        }
     }
 
     handleSubmit(event) {
 
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <div className="form-group">
+                        <label for="inputUserName">
+                            Username:
+                        </label>
+                        <input 
+                        type="email" 
+                        className="form-control" 
+                        id="inputUserName" />
+                    </div>
+                    <div className="form-group">
+                        <label for="passwordInput">
+                            Password:
+                        </label>
+                        <input 
+                        type="password" 
+                        className="form-control" 
+                        id="passwordInput" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        );
     }
 }
 
