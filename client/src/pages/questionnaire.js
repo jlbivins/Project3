fd61f4357cd7f7868a894d91aca44640d1ff0c72
Quyen Huynh <quyenh@hotmail.com>
2019-07-06 11:52:03 -0400

    package.json

--- client/src/pages/questionnaire.js
+++ client/src/pages/questionnaire.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Question from '../components/Question/question'
 
 function Questionnaire() {
-
+    return (
+        <React.Fragment>
+            <Question/>
+        </React.Fragment>
+    )
 };
 
 export default Questionnaire;
